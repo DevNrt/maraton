4aed67a7defa39ff5c5b44b77413eef1b282e6f5
nrodriguez_pm <nik891011@gmail.com>
2023-11-25 23:25:02 -0500

    customercrud

--- controllers/customers.js
+++ controllers/customers.js
@@ -1,11 +1,19 @@
 import db from "/Users/nicolasrodriguez/Downloads/maraton_back/databases/firebases.js";
-import { collection, addDoc } from "firebase/firestore";
+import {
+  collection,
+  addDoc,
+  updateDoc,
+  deleteDoc,
+  getDoc,
+  getDocs,
+  doc,
+  query,
+  where,
+} from "firebase/firestore";
+
+const firestore = db.database;
 
 async function createCustomer(req, res) {
-  const firestore = db.database;
-
-  const concursante = collection(firestore, "Concursante");
-
   if (
     req.body.nombre == "" ||
     req.body.apellido == "" ||
@@ -25,6 +33,7 @@ async function createCustomer(req, res) {
       correo: req.body.correo,
     };
 
+    const concursante = collection(firestore, "Concursante");
     await addDoc(concursante, datosConcursante)
       .then(() => {
         res.send("Notificación: Usuario creado!");
@@ -35,4 +44,191 @@ async function createCustomer(req, res) {
   }
 }
 
-export default { createCustomer };
+async function updateCustomer(req, res) {
+  const concursante = collection(firestore, "Concursante");
+  var variableCambiare = "";
+
+  if (req.body.nombreN != "") {
+    variableCambiare = req.body.nombreN;
+    const q = query(concursante, where("nombre", "==", req.body.nombreA));
+    const querySnapshot = await getDocs(q);
+    var id = " ";
+
+    querySnapshot.forEach((doc) => {
+      console.log(doc.id, " => ", doc.data());
+      id = doc.id;
+    });
+
+    const concursanteId = doc(firestore, "Concursante", id);
+    updateDoc(concursanteId, {
+      nombre: variableCambiare,
+    })
+      .then(() => {
+        res.send("Notificación: Usuario actualizado!");
+      })
+      .catch((error) => {
+        res.send(error);
+      });
+  } else if (req.body.apellidoN != "") {
+    variableCambiare = req.body.apellidoN;
+    const q = query(concursante, where("apellido", "==", req.body.apellidoA));
+    const querySnapshot = await getDocs(q);
+    var id = " ";
+
+    querySnapshot.forEach((doc) => {
+      console.log(doc.id, " => ", doc.data());
+      id = doc.id;
+    });
+
+    const concursanteId = doc(firestore, "Concursante", id);
+    updateDoc(concursanteId, {
+      apellido: variableCambiare,
+    })
+      .then(() => {
+        res.send("Notificación: Usuario actualizado!");
+      })
+      .catch((error) => {
+        res.send(error);
+      });
+  } else if (req.body.codigoN != "") {
+    variableCambiare = req.body.codigoN;
+    const q = query(concursante, where("codigo", "==", req.body.codigoA));
+    const querySnapshot = await getDocs(q);
+    var id = " ";
+
+    querySnapshot.forEach((doc) => {
+      console.log(doc.id, " => ", doc.data());
+      id = doc.id;
+    });
+
+    const concursanteId = doc(firestore, "Concursante", id);
+    await updateDoc(concursanteId, {
+      codigo: variableCambiare,
+    })
+      .then(() => {
+        res.send("Notificación: Usuario actualizado!");
+      })
+      .catch((error) => {
+        res.send(error);
+      });
+  } else if (req.body.carreraN != "") {
+    variableCambiare = req.body.carreraN;
+    const q = query(concursante, where("carrera", "==", req.body.carreraA));
+    const querySnapshot = await getDocs(q);
+    var id = " ";
+
+    querySnapshot.forEach((doc) => {
+      console.log(doc.id, " => ", doc.data());
+      id = doc.id;
+    });
+
+    const concursanteId = doc(firestore, "Concursante", id);
+    await updateDoc(concursanteId, {
+      carrera: variableCambiare,
+    })
+      .then(() => {
+        res.send("Notificación: Usuario actualizado!");
+      })
+      .catch((error) => {
+        res.send(error);
+      });
+  } else if (req.body.nivelN != "") {
+    variableCambiare = req.body.nivelN;
+    const q = query(concursante, where("nivel", "==", req.body.nivelA));
+    const querySnapshot = await getDocs(q);
+    var id = " ";
+
+    querySnapshot.forEach((doc) => {
+      console.log(doc.id, " => ", doc.data());
+      id = doc.id;
+    });
+
+    const concursanteId = doc(firestore, "Concursante", id);
+    await updateDoc(concursanteId, {
+      nivel: variableCambiare,
+    })
+      .then(() => {
+        res.send("Notificación: Usuario actualizado!");
+      })
+      .catch((error) => {
+        res.send(error);
+      });
+  } else if (req.body.correoN != "") {
+    variableCambiare = req.body.correoN;
+    const q = query(concursante, where("correo", "==", req.body.correoA));
+    const querySnapshot = await getDocs(q);
+    var id = " ";
+
+    querySnapshot.forEach((doc) => {
+      console.log(doc.id, " => ", doc.data());
+      id = doc.id;
+    });
+
+    const concursanteId = doc(firestore, "Concursante", id);
+    await updateDoc(concursanteId, {
+      correo: variableCambiare,
+    })
+      .then(() => {
+        res.send("Notificación: Usuario actualizado!");
+      })
+      .catch((error) => {
+        res.send(error);
+      });
+  }
+}
+
+async function readCustomer(req, res) {
+  const concursante = collection(firestore, "Concursante");
+  const q = query(concursante, where("codigo", "==", req.body.codigo));
+  const querySnapshot = await getDocs(q);
+  var data = "";
+  querySnapshot.forEach((doc) => {
+    console.log(doc.id, " => ", doc.data());
+    data = doc.data();
+  });
+  res.send(data);
+}
+
+async function readCustomers(req, res) {
+  const concursante = collection(firestore, "Concursante");
+  const q = query(
+    concursante,
+    where(req.body.parametro, "==", req.body.variable)
+  );
+  const querySnapshot = await getDocs(q);
+  var data = [];
+  var i = 0;
+  querySnapshot.forEach((doc) => {
+    console.log(doc.id, " => ", doc.data());
+    data[i] = doc.data();
+    i++;
+  });
+  res.send(data);
+}
+
+async function deleteCustomer(req, res) {
+  const concursante = collection(firestore, "Concursante");
+  const q = query(concursante, where("codigo", "==", req.body.codigo));
+  const querySnapshot = await getDocs(q);
+  var id = " ";
+  querySnapshot.forEach((doc) => {
+    console.log(doc.id, " => ", doc.data());
+    id = doc.id;
+  });
+  const concursanteId = doc(firestore, "Concursante", id);
+  await deleteDoc(concursanteId)
+    .then(() => {
+      res.send("Notificación: Usuario eliminado!");
+    })
+    .catch((error) => {
+      res.send(error);
+    });
+}
+
+export default {
+  createCustomer,
+  updateCustomer,
+  readCustomer,
+  readCustomers,
+  deleteCustomer,
+};
--- routes/croutes.js
+++ routes/croutes.js
@@ -5,10 +5,15 @@ import createCustomer from '../controllers/customers.js'
 
 api.post("/create",createUser.createUser)
 api.post("/createCustomer",createCustomer.createCustomer)
+api.post("/updateCustomer",createCustomer.updateCustomer)
+api.post("/readCustomer",createCustomer.readCustomer)
+api.post("/readCustomers",createCustomer.readCustomers)
+api.post("/deleteCustomer",createCustomer.deleteCustomer)
 api.post("/login",createUser.LoginUser)
 api.post("/delete",createUser.deleteU)
 api.post("/updateE",createUser.updateE)
 api.post("/updateP",createUser.updateP)
+
 
 
 
